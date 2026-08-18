ccaef7f398ff0c26bf20e078fb472f81ad4e20ed
linad <linad43merzlikin@yandex.com>
2026-08-19 00:17:58 +0300

    create tables, edit small fix, create for_shell.pyi

--- catalog/models.py
+++ catalog/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='images/', null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -31,11 +31,3 @@ class Product(models.Model):
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
         ordering = ['name']
-
-# products = [
-#     Product(name='Яблоко', price=10, category=category_food),
-#     Product(name='Груша', price=15, category=category_food),
-#     Product(name='Пицца', price=30, category=category_food),
-#     Product(name='Смартфон простой', price=5000, category=category_phone),
-#     Product(name='Крутой телефон', price=10000, category=category_phone)
-# ]
