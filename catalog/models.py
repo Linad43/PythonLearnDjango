3092afa42cefa83782cab0ebd75073d54ef31686
linad <linad43merzlikin@yandex.com>
2026-08-18 22:39:02 +0300

    edit in Product `created_at` and `updated_at`

--- catalog/models.py
+++ catalog/models.py
@@ -7,8 +7,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images/')
     category = models.ForeignKey
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} {str(self.price)}"
