862136d35466649041b1b5011a47f07aec256468
linad <linad43merzlikin@yandex.com>
2026-08-18 23:31:16 +0300

    edit models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,22 +1,5 @@
+from unicodedata import category
 from django.db import models
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='images/')
-    category = models.ForeignKey
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.name} {str(self.price)}"
-
-    class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
-        ordering = ['name']
 
 
 class Category(models.Model):
@@ -30,3 +13,29 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {str(self.price)}"
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ['name']
+
+# products = [
+#     Product(name='Яблоко', price=10, category=category_food),
+#     Product(name='Груша', price=15, category=category_food),
+#     Product(name='Пицца', price=30, category=category_food),
+#     Product(name='Смартфон простой', price=5000, category=category_phone),
+#     Product(name='Крутой телефон', price=10000, category=category_phone)
+# ]
