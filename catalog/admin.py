c8722f74ce5f9825a3f822c87afba5a12045a2cd
linad <linad43merzlikin@yandex.com>
2026-08-18 23:21:27 +0300

    admin.py commit register

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
